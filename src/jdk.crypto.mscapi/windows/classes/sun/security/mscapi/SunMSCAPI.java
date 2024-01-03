364a78c9f6a6e637b854b81d334f9f6e94d7f18b
Mat Carter <54955201+macarte@users.noreply.github.com>
2024-01-03 10:48:25 -0800

    8306688 Support Windows serialized keystores (SST files) (#16)
    * Windows SST support
    
    * hide SST support behind -Dsun.security.mscapi.enableSST=true
    
    * Create 8306688-sst.yml

--- src/jdk.crypto.mscapi/windows/classes/sun/security/mscapi/SunMSCAPI.java
+++ src/jdk.crypto.mscapi/windows/classes/sun/security/mscapi/SunMSCAPI.java
@@ -49,6 +49,8 @@ public final class SunMSCAPI extends Provider {
 
     private static final String INFO = "Sun's Microsoft Crypto API provider";
 
+    private static final String ENABLE_SST_PROP = "sun.security.mscapi.enableSST";
+
     static {
         @SuppressWarnings("removal")
         var dummy = AccessController.doPrivileged(new PrivilegedAction<Void>() {
@@ -99,6 +101,8 @@ public final class SunMSCAPI extends Provider {
                         return new CKeyStore.MYLocalMachine();
                     } else if (algo.equals("Windows-ROOT-LOCALMACHINE")) {
                         return new CKeyStore.ROOTLocalMachine();
+                    } else if (algo.equals("Windows-SST")) {
+                        return new CKeyStore.SST();
                     }
                 } else if (type.equals("Signature")) {
                     if (algo.equals("NONEwithRSA")) {
@@ -152,6 +156,9 @@ public final class SunMSCAPI extends Provider {
     public SunMSCAPI() {
         super("SunMSCAPI", PROVIDER_VER, INFO);
 
+        String enableSSTProp = AccessController.doPrivileged(
+            (PrivilegedAction<String>) () -> System.getProperty(ENABLE_SST_PROP));
+
         final Provider p = this;
         AccessController.doPrivileged(new PrivilegedAction<Void>() {
             public Void run() {
@@ -167,6 +174,10 @@ public final class SunMSCAPI extends Provider {
                 /*
                  * Key store
                  */
+                if ("true".equalsIgnoreCase(enableSSTProp)) {
+                    putService(new ProviderService(p, "KeyStore",
+                               "Windows-SST", "sun.security.mscapi.CKeyStore$SST"));
+                }
                 putService(new ProviderService(p, "KeyStore",
                            "Windows-MY", "sun.security.mscapi.CKeyStore$MY"));
                 putService(new ProviderService(p, "KeyStore",
