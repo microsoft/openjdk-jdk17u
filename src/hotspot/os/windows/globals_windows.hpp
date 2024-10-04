fcedfaa1e2efb87d6ec423afc3df03dc629e52cd
Dhamoder Nalla <dhanalla@openjdk.org>
2024-10-04 04:50:54 +0000

    8338136: Hotspot should support multiple large page sizes on Windows
    Reviewed-by: dholmes, djelinski

--- src/hotspot/os/windows/globals_windows.hpp
+++ src/hotspot/os/windows/globals_windows.hpp
@@ -36,6 +36,10 @@
                          range,                                           \
                          constraint)                                      \
                                                                           \
+product(bool, EnableAllLargePageSizesForWindows, false,                   \
+        "Enable support for multiple large page sizes on "                \
+        "Windows Server")                                                 \
+                                                                          \
 product(bool, UseOSErrorReporting, false,                                 \
         "Let VM fatal error propagate to the OS (ie. WER on Windows)")
 
