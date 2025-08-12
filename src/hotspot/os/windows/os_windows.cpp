3a7c0bd8744e88d5e119646717cf7cfc358d3551
Saint Wesonga <saint.wesonga@microsoft.com>
2025-08-12 17:24:02 -0600

    Windows AArch64 safefetch implementation should not use structured exception handling

--- src/hotspot/os/windows/os_windows.cpp
+++ src/hotspot/os/windows/os_windows.cpp
@@ -2675,6 +2675,10 @@ LONG WINAPI topLevelExceptionFilter(struct _EXCEPTION_POINTERS* exceptionInfo) {
     // Verify that OS save/restore AVX registers.
     return Handle_Exception(exceptionInfo, VM_Version::cpuinfo_cont_addr());
   }
+#elif defined(_M_ARM64)
+  if (handle_safefetch(exception_code, pc, (void*)exceptionInfo->ContextRecord)) {
+    return EXCEPTION_CONTINUE_EXECUTION;
+  }
 #endif
 
   if (t != NULL && t->is_Java_thread()) {
