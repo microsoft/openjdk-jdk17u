19a3e86c9b788c0ca61b62e997d157e836df24fe
Saint Wesonga <saint.wesonga@microsoft.com>
2025-09-09 16:57:18 -0600

    runtime/jni/nativeStack/TestNativeStack.java fails on Windows AArch64 (#15)

--- src/hotspot/os_cpu/windows_aarch64/os_windows_aarch64.inline.hpp
+++ src/hotspot/os_cpu/windows_aarch64/os_windows_aarch64.inline.hpp
@@ -27,4 +27,10 @@
 
 #include "runtime/os.hpp"
 
+#define HAVE_PLATFORM_PRINT_NATIVE_STACK 1
+inline bool os::platform_print_native_stack(outputStream* st, const void* context,
+                                     char *buf, int buf_size, address& lastpc) {
+  return os::win32::platform_print_native_stack(st, context, buf, buf_size, lastpc);
+}
+
 #endif // OS_CPU_WINDOWS_AARCH64_OS_WINDOWS_AARCH64_INLINE_HPP
