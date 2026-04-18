9ed26910734c75ac7bb5d3e945c1a26a3a9685bb
Java Platform Infrastructure <javaplatinfra@microsoft.com>
2026-04-18 06:01:05 +0000

    Merged feature 'ms-patches/reduce-allocation-merges'

--- src/hotspot/share/code/scopeDesc.hpp
+++ src/hotspot/share/code/scopeDesc.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -137,6 +137,7 @@ class ScopeDesc : public ResourceObj {
  public:
   // Verification
   void verify();
+  GrowableArray<ScopeValue*>* objects_to_rematerialize(frame& frm, RegisterMap& map);
 
 #ifndef PRODUCT
  public:
